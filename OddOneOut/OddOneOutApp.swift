42c2a2b2d5884df5546cf77d4c6a501d97b08e08
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-24 14:42:04 +0200

    Initial Commit

---
+++ OddOneOut/OddOneOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddOneOutApp.swift
+//  OddOneOut
+//
+//  Created by Sebastien REMY on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OddOneOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
